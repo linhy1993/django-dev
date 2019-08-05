4b1f2f90748517ca600fe69bcf5c474ba562c6d7
linhy1993 <linhongyi1110@gmail.com>
2019-08-05 00:02:04 -0400

    add crawl result table

--- maoyan/movies/views.py
+++ maoyan/movies/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from movies.spider import MovieSpider
+from movies.models import MaoyanMovie
+from django.core.paginator import Paginator, PageNotAnInteger, InvalidPage, EmptyPage
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'movies/index.html')
+    movies_lst = MaoyanMovie.objects.all()
+    if movies_lst:
+        paginator = Paginator(movies_lst, 10)
+        if request.method == 'GET':
+            page = request.GET.get('page')
+            movies = paginator.get_page(page)
+            return render(request, 'index.html', {'movies': movies, 'paginator': paginator, 'is_paginated': True})
+    return render(request, 'index.html')
 
 
 def spider(request):
-    spd = MovieSpider()
-    spd.run_spider()
-    return render(request, 'movies/spider.html')
+    if request.method == 'POST':
+        spd = MovieSpider()
+        spd.run_spider()
+        return redirect('index')
